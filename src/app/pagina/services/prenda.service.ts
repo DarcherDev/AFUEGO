d4526e1f287746391757826f9c462e3741c38243
DarcherDev <daniel_aristizabal@outlook.com>
2021-10-14 07:58:29 -0500

    crud cliente y prenda
    se crea crud en servicio de angular de prenda y cliente

--- src/app/pagina/services/prenda.service.ts
+++ src/app/pagina/services/prenda.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { Cliente } from '../interfaces/cliente.interface';
+import { Prenda } from '../interfaces/prenda.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PrendaService {
 
-  constructor() { }
+  private http: HttpClient;
+  private url : string = environment.baseUrl;
+
+  constructor( http: HttpClient) { 
+    this.http = http;
+  }
+  getPrenda(): Observable<Prenda[]>{
+    return this.http.get<Prenda[]>(`${this.url}/prenda`);    
+  }
+
+    // peticon get para obtener
+    getPrendaPorId(id:string): Observable<Prenda>{    
+      return this.http.get<Prenda>(`${this.url}/prenda/${ id }`);
+    }
+  
+    // peticon post para crear
+    agregarPrenda(prenda: Prenda): Observable<Prenda>{
+      return this.http.post<Prenda>(`${this.url}/prenda`, prenda);
+    }
+  
+    // peticon put para actualizar  
+    editarPrenda(prenda: Prenda): Observable<Prenda>{
+      return this.http.put<Prenda>(`${this.url}/prenda/${prenda.id}`, prenda);
+    }
+  
+    // peticon delete para eliminar   
+    BorrarPrenda(id: string): Observable<any>{
+      return this.http.delete<any>(`${this.url}/prenda/${id}`);
+    }
+
 }
